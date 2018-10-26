306319b415ceb1892e05717d5172c88e275d3496
sommersoft <sommersoft@gmail.com>
2018-10-26 17:12:35 -0500

    conf.py: only necessary intersphinx for busdevice & register; thanks carter for bringing this up

--- {{ cookiecutter.library_name }}/docs/conf.py
+++ {{ cookiecutter.library_name }}/docs/conf.py
@@ -24,10 +24,10 @@ extensions = [
 
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),
-                        {%- if cookiecutter.depends_on_bus_device -%}
+                        {%- if cookiecutter.depends_on_bus_device in ["y", "yes"] -%}
                         'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),
                         {%- endif -%}
-                        {%- if cookiecutter.depends_on_register -%}
+                        {%- if cookiecutter.depends_on_register ["y", "yes"] -%}
                         'Register': ('https://circuitpython.readthedocs.io/projects/register/en/latest/', None),
                         {%- endif -%}
                         'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
