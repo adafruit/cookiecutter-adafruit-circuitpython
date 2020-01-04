c82e06c0d56e481496cf32c177f61ed9440903b9
sommersoft <sommersoft@gmail.com>
2020-01-04 13:13:54 -0600

    debug and docstring cleanup.

--- tests/test_make_cookies.py
+++ tests/test_make_cookies.py
@@ -24,16 +24,21 @@ with open(cookie_json_path.resolve(), 'r') as cookie_file:
 
 def overwrite_workaround():
     """ cookiecutter's `overwrite_if_exists=True` is currently broken.
-        There is an accepted PR slated for the release 1.8.0, but until
+        There is an accepted PR slated for release 1.8.0, but until
         it is released, we'll need to work around it.
     """
     if output_dir.exists():
         shutil.rmtree(str(output_dir.resolve()))
 
 
-def compare_template_dirs(library_name='test', library_prefix=None):
+def compare_template_dirs(*, library_name='test', library_prefix=None):
     """ Helper function to compare the results of generated files,
         to the available files in the template.
+
+        :param: str library_name: Name used for the template's `library_name`
+                                  prompt. Default: 'test'
+        :param: None,str library_prefix: The value used for the template's
+                                         `library_prefix` prompt. Default: None
     """
 
     cookie_template_path = working_dir / '{{ cookiecutter.library_name }}'
@@ -64,9 +69,9 @@ def compare_template_dirs(library_name='test', library_prefix=None):
 
     return 0 <= len(template_files.difference(set(generated_files)))
 
-def test_new_cookiecutter_only_required_entries(capsys):
-    """ Basic test of running cookiecutter, supplying only the required
-        info.
+def test_new_cookiecutter_only_required_entries():
+    """ Basic test of running cookiecutter, supplying info only to the
+        required fields (cookiecutter.json values of 'null').
     """
 
     # delete testing output directory if exists
@@ -76,6 +81,8 @@ def test_new_cookiecutter_only_required_entries(capsys):
     for key, value in cookie_json.items():
         if value == None:
             test_context[key] = 'test'
+        if key.startswith('_'):
+            test_context[key] = value
 
     new_cookie = cookiecutter(
         str(working_dir.resolve()),
@@ -88,9 +95,10 @@ def test_new_cookiecutter_only_required_entries(capsys):
     assert new_cookie == '{}/test'.format(output_dir.resolve())
     assert compare_template_dirs()
 
-def test_new_cookiecutter_all_entries(capsys):
-    """ Basic test of running cookiecutter, supplying only the required
-        info.
+def test_new_cookiecutter_all_entries():
+    """ Basic test of running cookiecutter, supplying info for all fields.
+        All fields will have 'test' as their value, so this is only a
+        minimal & cursory test.
     """
 
     # delete testing output directory if exists
