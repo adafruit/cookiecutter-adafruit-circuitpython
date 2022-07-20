e213376b040d887798bdc7a7a6e2ba511de5b71f
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-07-20 15:27:00 -0400

    Fix version check logic

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,6 +6,6 @@ semver = cookiecutter.__version__
 major, minor, _ = semver.split(".")
 maj_min_ver = float(major + "." + minor)
 
-if maj_min_ver >= MIN_VERSION:
+if maj_min_ver < MIN_VERSION:
     print("cookiecutter must be at a minimum of version", MIN_VERSION)
     raise SystemExit(1)
